4b0ca4ba50b41025f53efe3869bda040c19c691f
OlexDragon <OlexDragon@6c93db66-e737-48da-b1bb-0cfd2fa6248b>
2018-11-14 18:28:58 +0000

    Deleted unused classes

--- flash_2/src/main/java/irt/flash/presentation/panel/edit_profile/table/TemperatureTableModel.java
+++
@@ -1,9 +0,0 @@
-package irt.flash.presentation.panel.edit_profile.table;
-
-public class TemperatureTableModel extends AbstractTableModel {
-	private static final long serialVersionUID = -8345853888061797248L;
-
-	public TemperatureTableModel() {
-		super(new String[]{"Temperature ( C )" , "DAC Value"});
-	}
-}
